3cda121b7b3db9e820ee54b87aa0305df189a580
hesom <5159852+hesom@users.noreply.github.com>
2018-10-03 20:01:11 +0200

    Add laser prefab

---
+++ UnityProjects/Fusion/Assets/_VirtualSelf/Scripts/Laser/Laser.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+    private LineRenderer lr;
+
+	// Use this for initialization
+	void Start () {
+        lr = GetComponent<LineRenderer>();
+        lr.useWorldSpace = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lr.SetPosition(0, transform.position);
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.right, out hit))
+        {
+            if (hit.collider)
+            {
+                lr.SetPosition(1, hit.point);
+            }
+        }
+        else
+        {
+            lr.SetPosition(1, transform.right * 5000);
+        }
+	}
+}
