5a04110e2f9da7ea4c39b87e16fee2937ad4ce0d
hesom <5159852+hesom@users.noreply.github.com>
2018-10-03 15:54:26 +0200

    Add callback script that automatically counts the missing keys

---
+++ UnityProjects/Fusion/Assets/_VirtualSelf/Scripts/Keypad/KeypadCompleted.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Leap.Unity.Interaction;
+
+public class KeypadCompleted : MonoBehaviour {
+
+    public UnityEvent OnKeypadCompleted;
+
+    private int missingKeyCount;
+
+	// Use this for initialization
+	void Start () {
+        var missingKeys = GetComponentsInChildren<AnchorableBehaviour>();
+        missingKeyCount = missingKeys.Length;
+	}
+
+    public void KeySetIn()
+    {
+        missingKeyCount--;
+        if(missingKeyCount == 0)
+        {
+            OnKeypadCompleted.Invoke();
+        }
+        if(missingKeyCount < 0)
+        {
+            throw new System.Exception("More keys set into tutorial keypad than there should have been in the scene!");
+        }
+    }
+}
