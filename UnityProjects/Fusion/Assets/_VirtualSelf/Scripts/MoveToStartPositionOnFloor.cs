387cc7b8b71850071dde2aba12fd9b1ae3be90eb
hesom <5159852+hesom@users.noreply.github.com>
2018-10-03 16:28:59 +0200

    Make keys respawn if they hit the floor in tutorial scene

---
+++ UnityProjects/Fusion/Assets/_VirtualSelf/Scripts/MoveToStartPositionOnFloor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToStartPositionOnFloor : MonoBehaviour {
+
+    private Vector3 startPos;
+    private Quaternion startRot;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        startPos = transform.position;
+        startRot = transform.rotation;
+        rb = GetComponent<Rigidbody>();
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "Floor")
+        {
+            transform.position = startPos;
+            transform.rotation = startRot;
+            rb.velocity = Vector3.zero;
+        }
+    }
+}
