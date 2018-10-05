a9207378f649649874aa2e2ab4b4a788896a24b4
PGVirtualSelf1718 <PGVirtualSelf1718@gmail.com>
2018-10-05 19:07:11 +0200

    Add portal spawn prefab to every scene and keypad to MasterScene

---
+++ UnityProjects/Fusion/Assets/_VirtualSelf/Scripts/Keypad/ActivateKeypad.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ActivateKeypad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() {
+		var obj = SceneManager.GetSceneByName("MasterScene").GetRootGameObjects();
+		foreach (var o in obj) {
+			if (o.tag == "Keypad") {
+				o.SetActive(true);
+			}
+		}
+	}
+
+}
---
+++ UnityProjects/Fusion/Assets/_VirtualSelf/Scripts/Keypad/DeactivateKeypadInTutorial.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateKeypadInTutorial : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var keypad = GameObject.FindWithTag("Keypad");
+		keypad.SetActive(false);
+	}
+}
