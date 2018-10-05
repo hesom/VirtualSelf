7843eeadb86bda4e5d0bd425dab25858a239bfa4
john <john.rickard@student.uni-siegen.de>
2018-10-05 15:56:40 +0200

    Rename Script and Add to new Prefab for Body Tracking controller

--- UnityProjects/Fusion/Assets/_VirtualSelf/Scripts/BodyRotations/RotationAdapter.cs
+++ UnityProjects/Fusion/Assets/_VirtualSelf/Scripts/BodyRotations/RotationAdapter.cs
@@ -1,30 +1,23 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 
 namespace VirtualSelf.BodyRotations
 {
-    public class AdaptTrackedRotation : MonoBehaviour
+    public class RotationAdapter : MonoBehaviour
     {
         public BodyRotationData Rotation;
+
+        public GameObject MeshGameObject => childMeshTransform.gameObject;
 
         private Transform childMeshTransform;
 
         // Use this for initialization
         private void Start()
         {
-            childMeshTransform = transform.GetChild(0);
+            childMeshTransform = transform.GetComponentInChildren<MeshRenderer>()?.transform;
             if (childMeshTransform == null)
-                Debug.LogError("The tracked gameobject has to have its real mesh as a child.");
+                throw new UnityException("The tracked gameobject has to have its real mesh as a child.");
             childMeshTransform.localRotation = Rotation.RelativeInverseRotation;
-        }
-
-        // Update is called once per frame
-        private void Update()
-        {
-            //Debug.Log(transform.localRotation);
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                SaveInverseRotation();
-            }
         }
 
         /// <summary>
@@ -32,11 +25,26 @@ namespace VirtualSelf.BodyRotations
         /// Acts nearly like a "freeze transformations" for the childmesh.
         /// Inverse rotation is saved in ScriptableObject to preserve state at startup.
         /// </summary>
-        private void SaveInverseRotation()
+        public void SaveInverseRotation()
         {
             var inverse = Quaternion.Inverse(transform.localRotation);
             childMeshTransform.localRotation = inverse;
             Rotation.RelativeInverseRotation = inverse;
+        }
+    }
+
+    [CustomEditor(typeof(RotationAdapter))]
+    public class SaveRotationEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+        
+            var myScript = (RotationAdapter)target;
+            if (GUILayout.Button("Save Inverse Rotation"))
+            {
+                myScript.SaveInverseRotation();
+            }
         }
     }
 }
