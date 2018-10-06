63edb75659a3d2579065d0476cab8b3be454605a
Blacklands <Blacklands@gmx.net>
2018-10-06 16:34:07 +0200

    Add a utility file containing all UnityEvents visible in the Editor
    "UnityEvent" is powerful, and can take up to four arguments of any
    type. But: This is only true for runtime code. Unity can only draw
    UnityEvents with the following properties:
    - Zero or one argument
    - Argument type is one of: bool, int, float string,
    UnityEngine.Object
    
    Also, Unity cannot serialize generic types. One has to inherit from
    a specific generic type instead. This is what this file contains,
    the UnityEvent types inheriting from the generic types with the
    arguments mentioned above.

--- UnityProjects/Fusion/Assets/_VirtualSelf/Scripts/Utility/Unity/UnityEvents.cs
+++ UnityProjects/Fusion/Assets/_VirtualSelf/Scripts/Utility/Unity/UnityEvents.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine.Events;
 
 
@@ -8,29 +9,34 @@ namespace VirtualSelf.Utility.UnityEvents {
 /// A <see cref="UnityEvent"/> class with one argument of type <c>bool</c>. This UE can be used in
 /// Unity Editors (e.g. Inspectors).
 /// </summary>
+[Serializable]
 public sealed class BoolUE : UnityEvent<bool> { }
     
 /// <summary>
 /// A <see cref="UnityEvent"/> class with one argument of type <c>int</c>. This UE can be used in
 /// Unity Editors (e.g. Inspectors).
 /// </summary>
+[Serializable]
 public sealed class IntUE : UnityEvent<int> { }
     
 /// <summary>
 /// A <see cref="UnityEvent"/> class with one argument of type <c>float</c>. This UE can be used in
 /// Unity Editors (e.g. Inspectors).
 /// </summary>
+[Serializable]
 public sealed class FloatUE : UnityEvent<float> { }
 
 /// <summary>
 /// A <see cref="UnityEvent"/> class with one argument of type <c>string</c>. This UE can be used in
 /// Unity Editors (e.g. Inspectors).
 /// </summary>
+[Serializable]
 public sealed class StringUE : UnityEvent<string> { }
     
 /// <summary>
 /// A <see cref="UnityEvent"/> class with one argument of type <see cref="UnityEngine.Object"/>.
 /// This UE can be used in Unity Editors (e.g. Inspectors).
 /// </summary>
+[Serializable]
 public sealed class ObjectUE : UnityEvent<UnityEngine.Object> { }
 }
