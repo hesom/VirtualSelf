ce34734861d8636d81de9c920455a749dcf61ffe
hesom <5159852+hesom@users.noreply.github.com>
2018-10-07 21:35:10 +0200

    Add script to forward PortalTraversed event from SceneSwitcher to specific scene at runtime

---
+++ UnityProjects/Fusion/Assets/_VirtualSelf/Scripts/PortalTraversedForwarder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+namespace VirtualSelf
+{
+    public class PortalTraversedForwarder : MonoBehaviour
+    {
+        public SceneSwitcher sceneSwitcher;
+        public UnityEvent onPortalTraversed;
+        // Use this for initialization
+        void Start()
+        {
+            sceneSwitcher.onPortalTraversed.AddListener(ForwardEvent);
+        }
+
+        public void ForwardEvent()
+        {
+            onPortalTraversed.Invoke();
+        }
+    }
+}
