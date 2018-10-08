266645fae7de2244db107b9eb6fe781d6d6a385a
Blacklands <Blacklands@gmx.net>
2018-10-08 08:51:20 +0200

    Add a (totally unfinished) manager class for the Information Board
    This doesn't really have anything yet. I was still figuring out how
    to best do it.

---
+++ UnityProjects/Fusion/Assets/_VirtualSelf/Scripts/InformationBoard/InfoBoardManager.cs
@@ -0,0 +1,53 @@
+using VirtualSelf.Utility;
+
+namespace VirtualSelf.GameSystems {
+
+
+/// <summary>
+/// TODO: Fill out this class description: InfoBoardManager
+/// </summary>
+public sealed class InfoBoardManager {
+
+    /* ---------- Variables & Properties ---------- */
+
+    public LampAnimator LeftLight;
+    public LampAnimator RightLight;
+
+    public Room room;
+    
+    
+
+
+
+
+    /* ---------- Constructors ---------- */
+
+
+
+
+
+
+    /* ---------- Methods ---------- */
+
+
+
+
+
+
+    /* ---------- Overrides ---------- */
+
+
+
+
+
+
+    /* ---------- Inner Classes ---------- */
+
+
+
+
+
+
+}
+
+}
