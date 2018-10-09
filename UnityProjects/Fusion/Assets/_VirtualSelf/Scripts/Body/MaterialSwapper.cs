1ac4d2d1698a2d0b25514da4432586c9dcbbae78
manuel_j_ <manuel.juengst@student.uni-siegen.de>
2018-10-10 00:50:45 +0200

    Create ShoulderGenerator, create MaterialSwapper for CollisionConnectors, ...
    - add the MaterialSwapper to DebugGesture
    
    The DebugGesture now contains:
    - Capsule Hands
    - shows skip buttons
    - shows body colliders

---
+++ UnityProjects/Fusion/Assets/_VirtualSelf/Scripts/Body/MaterialSwapper.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VirtualSelf
+{
+	
+public class MaterialSwapper : MonoBehaviour
+{
+	public Material Material;
+	public Material MatAlt;
+	public CollisionConnector[] CollisionConnectors;
+	public Renderer[] OtherMaterials;
+
+	private Material _copy;
+
+	void Awake()
+	{
+		_copy = MatAlt;
+	}
+
+#if UNITY_EDITOR
+	void OnValidate()
+	{
+		UnityEditor.EditorApplication.delayCall += Apply;
+	}
+#endif
+
+	public void Apply()
+	{
+		Apply(Material);
+	}
+	
+	public void Apply(Material m)
+	{
+		Material = m;
+		foreach (CollisionConnector c in CollisionConnectors)
+		{
+			c.Advanced.Material = Material;
+		}
+
+		foreach (Renderer ren in OtherMaterials)
+		{
+			ren.material = Material;
+			ren.enabled = Material != null;
+		}
+	}
+
+	public void SwapAlt()
+	{
+		if (_copy == MatAlt)
+		{
+			_copy = Material;
+			Apply(MatAlt);
+		}
+		else
+		{
+			// Material is MatAlt
+			// MatAlt is MatAlt
+			// _copy is Material
+			Material = _copy;
+			_copy = MatAlt;
+			Apply();
+		}
+	}
+}
+
+}
---
+++ UnityProjects/Fusion/Assets/_VirtualSelf/Scripts/Body/ShoulderGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VirtualSelf
+{
+	
+public class ShoulderGenerator : MonoBehaviour
+{
+
+	public Transform Neck;
+	public Transform Left, Right;
+	public Transform LeftUpperArm, RightUpperArm;
+
+	private float _y;
+	private float _width;
+	
+	void Start ()
+	{
+		// get y offset and shoulder point distance based on initial shoulder positions (averaged) relative to neck
+		_y = Neck.position.y - (Left.position.y + Right.position.y) / 2;
+		Vector3 neckPlane = Neck.position;
+		Vector3 lP = Left.position;
+		Vector3 rP = Right.position;
+		neckPlane.y = 0;
+		lP.y = 0;
+		rP.y = 0;
+		_width = (Vector3.Distance(neckPlane, lP) + Vector3.Distance(neckPlane, rP)) / 2;
+	}
+	
+	void Update ()
+	{
+		Vector3 flat = Vector3.ProjectOnPlane(LeftUpperArm.position - RightUpperArm.position, Vector3.up).normalized;
+		Vector3 neckdown = Neck.position;
+		neckdown.y -= _y;
+		Left.position = neckdown + flat * _width;
+		Right.position = neckdown + flat * -_width;
+	}
+}
+
+}
