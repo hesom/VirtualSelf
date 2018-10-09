08611768598076d32848407cfd3ff24cbece76dc
manuel_j_ <manuel.juengst@student.uni-siegen.de>
2018-10-10 01:05:07 +0200

    Create wrists that (should) disconnect from the body colliders on tracking loss

---
+++ UnityProjects/Fusion/Assets/_VirtualSelf/Scripts/Body/FleeOnTrackingLoss.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Leap.Unity;
+using Leap.Unity.Interaction;
+using UnityEngine;
+
+namespace VirtualSelf
+{
+
+public class FleeOnTrackingLoss : MonoBehaviour
+{
+	public InteractionHand Hand;
+	private bool _lastTracked = true;
+	
+//	void Start ()
+//	{
+//		HandModel.OnBegin += () => transform.localPosition = Vector3.zero;
+//		HandModel.OnFinish += () => transform.localPosition = Vector3.one * 10000;
+//	}
+
+	void Update()
+	{
+		if (Hand.isTracked != _lastTracked)
+		{
+			transform.localPosition = Hand.isTracked ? Vector3.zero : Vector3.one * 10000;
+			_lastTracked = Hand.isTracked;
+		}
+	}
+}
+
+}
