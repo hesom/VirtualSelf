ecc168eb3553666ee1240bc3ced687624adec2c7
manuel_j_ <manuel.juengst@student.uni-siegen.de>
2018-10-10 02:10:18 +0200

    Add bool to SwitchDelayed to keep track of active debug mode, query this on skip buttons

---
+++ UnityProjects/Fusion/Assets/QueryDebugMode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VirtualSelf
+{
+
+[RequireComponent(typeof(DisableSet))]
+public class QueryDebugMode : MonoBehaviour
+{
+	public SwitchDelayed SwitchDelayed;
+	
+	// Use this for initialization
+	void Start () {
+		// by default, we assume debug mode is on, which means the DisableSet script should be active and thus also
+		// the skip button. only if we are not in debug mode, we need to disable our disableset.
+		// though there should be no harm in setting it both cases anyways
+		GetComponent<DisableSet>().enabled = SwitchDelayed.IsDebug;
+	}
+}
+
+}
--- UnityProjects/Fusion/Assets/_VirtualSelf/Scripts/SwitchDelayed.cs
+++ UnityProjects/Fusion/Assets/_VirtualSelf/Scripts/SwitchDelayed.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 using VirtualSelf;
 
 [RequireComponent(typeof(DebounceEvents))]
-public class SwitchDelayed : MonoBehaviour {
+public class SwitchDelayed : MonoBehaviour
+{
+	public bool IsDebug { get; private set; }
 
 	// Use this for initialization
-	void Start () 
+	void Start ()
 	{
+		IsDebug = true;
+		GetComponent<DebounceEvents>().Event.AddListener(() => IsDebug = !IsDebug);
 		Invoke(nameof(Stuff), 1);
 	}
 
 	private void Stuff() {
 //		GetComponent<ModelSwitcher>().CycleGroup();
 		GetComponent<DebounceEvents>().TryTigger();
+		IsDebug = false;
 	}
 }
