599cb1640640a3a2e4fdf1b3405d1ddaf4cc48ab
hesom <5159852+hesom@users.noreply.github.com>
2018-10-11 01:18:38 +0200

    Change portal fader script to move the portal down instead of fading

--- UnityProjects/Fusion/Assets/_VirtualSelf/Scripts/SceneSwitcher/Fader.cs
+++ UnityProjects/Fusion/Assets/_VirtualSelf/Scripts/SceneSwitcher/Fader.cs
@@ -6,11 +6,15 @@ public class Fader : MonoBehaviour
 {
     public Material fadeMaterial;
     public float fadeDuration;
+    public bool moveDown = true;
 
+    private Vector3 initialPosition;
+    private Transform portalFrame;
     public void StartFade()
     {
-        var frame = transform.Find("PortalMesh").Find("Frame");
-        foreach (Transform child in frame)
+        portalFrame = transform.Find("PortalMesh").Find("Frame");
+        initialPosition = portalFrame.position;
+        foreach (Transform child in portalFrame)
         {
             var renderer = child.gameObject.GetComponent<MeshRenderer>();
             renderer.material = fadeMaterial;
@@ -25,9 +29,18 @@ public class Fader : MonoBehaviour
         while (t < fadeDuration)
         {
             t += Time.deltaTime;
-            float blend = Mathf.Clamp01(t / fadeDuration);
-            color.a = Mathf.Lerp(1.0f, 0.0f, blend);
-            mat.color = color;
+            if (!moveDown)
+            {
+                float blend = Mathf.Clamp01(t / fadeDuration);
+                color.a = Mathf.Lerp(1.0f, 0.0f, blend);
+                mat.color = color;
+            }
+            else
+            {
+                t += Time.deltaTime;
+                float lerp = Mathf.Lerp(0.0f, 2.0f, t);
+                portalFrame.position = initialPosition + lerp * Vector3.down;
+            }
 
             yield return null;
         }
