1297e190116b9b7921918f0e706891364eb5fedc
PGVirtualSelf1718 <PGVirtualSelf1718@gmail.com>
2018-10-11 21:58:13 +0200

    Remove elbow renderers, fix skip button box

---
+++ UnityProjects/Fusion/Assets/LayerOnEnable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerOnEnable : MonoBehaviour {
+
+	public string Layer;
+	
+	private void OnEnable() {
+		int layer = LayerMask.NameToLayer(Layer);
+		gameObject.layer = layer;
+		foreach (Transform t in transform) {
+			t.gameObject.layer = layer;
+		}
+	}
+
+}
