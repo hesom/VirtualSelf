c0e499bd2b517bfbfa844a51e49d9e5636e854ad
hesom <5159852+hesom@users.noreply.github.com>
2018-10-11 03:00:38 +0200

    Connect skip button with right keycode or learned gesture in every scene

---
+++ UnityProjects/Fusion/Assets/_VirtualSelf/Scripts/UnlockFireBeam.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockFireBeam : MonoBehaviour {
+
+    private GlobalFireGestureState globalGameState;
+    private void Start()
+    {
+        globalGameState = GameObject.FindGameObjectWithTag("GlobalGameState")?.GetComponent<GlobalFireGestureState>();
+        if(globalGameState == null)
+        {
+            throw new System.Exception("No GlobalGameState object for fire gesture found");
+        }
+    }
+
+    public void GetFire()
+    {
+        globalGameState.fireGestureDiscovered = true;
+    }
+}
